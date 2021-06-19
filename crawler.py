c9245537e2ed706784a05b04c5239383fbbe3d9c
kating <t141826@gmail.com>
2021-06-19 20:49:42 +0800

    dcardtest3

--- crawler.py
+++ crawler.py
@@ -11,7 +11,9 @@ class crawler:
     chrome_options.add_argument("--disable-dev-shm-usage")
     self.__driver=webdriver.Chrome(chrome_options=chrome_options)
     self.__information="""
-      dcard Linebot v1
+      *dcard Linebot v1
+      create on 2021/6/19
+      By Apple
     """
   # __information回傳的資訊
   @property
@@ -41,13 +43,17 @@ class crawler:
     sl(0.5)
     r_list=self.__driver.find_elements_by_xpath(' //*[@id="__next"]/div[2]/div[2]/div/div/div/div[2]/div[2]/div/div[1]/div')
     xStr=""
+    notArticleList=["公告","版規","瀏覽此看板"]
     for article in r_list:
       try:
-        title=article.find_element_by_xpath('./article/h2/a/span').text
-        href=article.find_element_by_xpath('./article/h2/a').get_attribute('href')
-        motion=article.find_element_by_xpath('./article/div[3]/div[1]/div/div[2]').text
-        response=article.find_element_by_xpath('./article/div[3]/div[2]/div/span').text
-        xStr +="\n".join([title,href,motion,response])
+        title="標題:"+article.find_element_by_xpath('./article/h2/a/span').text
+        if [i for i in notArticleList if i in title]:
+          continue
+        hyperLink="連結:"+article.find_element_by_xpath('./article/h2/a').get_attribute('href')
+        feel="心情:"+article.find_element_by_xpath('./article/div[3]/div[1]/div/div[2]').text
+        answer="回應:"+article.find_element_by_xpath('./article/div[3]/div[2]/div/span').text
+        articleString="\n".join([title,hyperLink,feel,answer])
+        xStr += articleString+"\n"+('-'*28)+"\n"
       except Exception as e:
         pass
     self.__close()
